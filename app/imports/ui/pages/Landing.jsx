cc4837e04db99107f6199c8d4d98e81e14db353e
Aaron Ancheta <al.ancheta10@gmail.com>
2023-11-07 15:26:51 -1000

    Update
    Expanded the about section and created a mission section

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { Col, Button, Container, Row } from 'react-bootstrap';
+import { Col, Button, Container, Row, Card } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <>
     <div className="landing-part1">
-      <Container id="landing-page" fluid className="py-3 text-center">
-        <h1>Reduce, Reuse, Repeat</h1>
+      <Container id="landing-page" className="py-3 text-center">
+        <Row>
+          <Col>
+            <h2 className="text-center">What is Sustainer?</h2>
+            <p>
+              {/* eslint-disable-next-line max-len */}
+              Single-use takeout containers and food wrappers make up a significant portion—up to one-third—of the debris discovered along Hawai’i&apos;s shorelines. Sustainer is dedicated to tackling this environmental challenge by providing a convenient and user-friendly reusable takeout container system. Our aim is to contribute to reducing this prevalent waste and eliminating debrees along Hawai’i&apos;s coastal areas. Join us in making a positive impact by adopting our sustainable solution for takeout packaging.
+            </p>
+          </Col>
+        </Row>
       </Container>
-
+      <Container className="py-3" />
       <Container>
-        <Row className="justify-content-center">
+        <Row className="justify-content-center my-sm-auto">
           <Col className="g-0" xs="auto">
             <Button className="rounded-0" variant="outline-light" size="lg" href="/signin">
               Sign In
@@ -27,18 +35,28 @@ const Landing = () => (
 
     <div className="landing-part2">
       <Container className="my-md-auto my-sm-auto my-lg-auto mx-sm-auto">
-        <Row>
-          <Col>
-            <h2>What is Sustainer?</h2>
-            <p>
-              Single-use takeout containers and food wrappers account for up to one-third
-              of the debris found on Hawai’i’s shorelines! Sustainer helps to address this
-              issue by offering an easy to use reusable takeout container
-              system.
-            </p>
+        <Row className="justify-content-center">
+          <Col lg="auto" md="auto" xs="auto">
+            <Card style={{ width: '14em' }}>
+              <Card.Body>
+                <Card.Title>Reduce</Card.Title>
+                <Card.Text>
+                  Minimize container loss and enhance the successful implementation of the Full Cycle Takeout program.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg="auto" md="auto" xs="auto">
+            <Card style={{ width: '14em' }}>
+              <Card.Body>
+                <Card.Title>Reuse</Card.Title>
+                <Card.Text>
+                  Establish a streamlined system to guarantee the return of all containers, facilitating their reuse and sustainability.
+                </Card.Text>
+              </Card.Body>
+            </Card>
           </Col>
         </Row>
-        <Row />
       </Container>
     </div>
   </>
