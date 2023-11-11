e71e8dbb29ad0df501d4e096bd875db33434fa06
Jaren Pinera <jpinera@hawaii.edu>
2023-11-10 18:37:55 -1000

    Update Mongo.js

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,16 +1,15 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
 import { Containers } from '../../api/container/Containers.js';
 import { VendorOrder } from '../../api/vendor/VendorOrder';
 
 /* eslint-disable no-console */
 
+/**
 // Initialize the database with a default data document.
 const addData = (data) => {
   console.log(`  Adding: ${data.name} (${data.owner})`);
   Stuffs.collection.insert(data);
 };
-
 // Initialize the StuffsCollection if empty.
 if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
@@ -18,29 +17,31 @@ if (Stuffs.collection.find().count() === 0) {
     Meteor.settings.defaultData.forEach(data => addData(data));
   }
 }
+*/
 
-// Initialize the database with a default data document.
+// Initialize the database with a default container document.
 const addContainer = (data) => {
   console.log(`  Adding: ${data.owner} (${data.size})`);
   Containers.collection.insert(data);
 };
 
-// Initialize the StuffsCollection if empty.
+// Initialize the Containers collection if empty.
 if (Containers.collection.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
+  if (Meteor.settings.defaultContainer) {
     console.log('Creating default containers.');
     Meteor.settings.defaultContainer.forEach(data => addContainer(data));
   }
 }
 
+// Initialize the database with a default vendorOrder document.
 const addVendorOrder = (data) => {
-  console.log(`  Adding: ${data.owner} (${data.size})`);
+  console.log(`  Adding: ${data.email} (${data.containers})`);
   VendorOrder.collection.insert(data);
 };
 
-// Initialize the StuffsCollection if empty.
+// Initialize the VendorOrder if empty.
 if (VendorOrder.collection.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
+  if (Meteor.settings.defaultOrders) {
     console.log('Creating default containers.');
     Meteor.settings.defaultOrders.forEach(data => addVendorOrder(data));
   }
